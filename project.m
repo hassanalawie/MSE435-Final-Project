a0c03e140411c8dfd3c78590500d17d4a05f6dbd
hassanalawie <hassan.alawie10@gmail.com>
2025-03-22 23:41:36 -0400

    slower subproblem approach

--- project.m
+++ project.m
@@ -1,44 +1,37 @@
-%%% RESTRICTED MASTER PROBLEM %%%%
-
+%%% RESTRICTED MASTER PROBLEM %%%
+tic;  % Start timing the entire script
 filename = 'FlightLegs.csv';
 M = readtable(filename);
 M = addArrivalDate(M);
 
 % Generate single-leg pairings
-% This will give us a 1 x 2512 struct where each struct has properties
-% PairingID(string), 
-% Legs (1 x 1 struct with properties Date, Duration, DepartureTime, ArrivalTime, DepartureAirport, ArrivalAirport, Aircraft, Airline, FlightNumber), 
-% Cost (float)
- disp("Generating Initial Pairings...")
- initial_pairings = generate_initial_pairings(M);
- disp("Initial Pairings Generated")
-% Generate multi-leg pairings
-% I DITCHED THIS BECAUSE WE DON'T NEED THESE FOR INITIAL, too much work
-% multi_leg_pairings = generate_feasible_pairings(M);
-% Combine both single-leg and multi-leg pairings
-% all_pairings = [initial_pairings, multi_leg_pairings];
+disp("Generating Initial Pairings...");
+pairingStartTime = tic;  % Start timing initial pairing generation
+initial_pairings = generate_initial_pairings(M);
+fprintf("Initial Pairings Generated (%.2f seconds)\n", toc(pairingStartTime));
 
-
+% Generate multi-leg pairings (Skipped for now)
 all_pairings = initial_pairings;
 
-% Generate sigma matrix. Sigma is a matrix that says if flight f is covered
-% by pairing p, in this case initially it is an N x N identity matrix,
-% where N is the number of flights
+% Generate sigma matrix
+sigmaStartTime = tic;
 sigma = generate_sigma(all_pairings, M);
+fprintf("Sigma Matrix Generated (%.2f seconds)\n", toc(sigmaStartTime));
 
 % Solve RMP
+rmpStartTime = tic;
 c = [all_pairings.Cost]'; 
-% This will just call linprog and return the relevant variables
 [xOpt, fval, exitflag, output, dualVariables] = solve_rmp_lp(c, sigma);
+fprintf("RMP Solved (%.2f seconds)\n", toc(rmpStartTime));
 
 % Compute reduced costs
 reducedCosts = c - sigma' * dualVariables;
 
-
-%%% SUBPROBLEM %%%%
-disp("Building Adjacency List...")
+%%% SUBPROBLEM %%%
+disp("Building Adjacency List...");
+adjListStartTime = tic;
 adjList = build_adjlist(M);
-disp("Adjacency List Built")
+fprintf("Adjacency List Built (%.2f seconds)\n", toc(adjListStartTime));
 
 % Prepare parameters for subproblem
 costParams.deadheadCost = 3000;
@@ -49,6 +42,10 @@ costParams.maxDailyFlight = 8;
 costParams.maxDutyDays = 4;
 costParams.minRestHours = 9;
 
-
+% Solve the subproblem
+subproblemStartTime = tic;
 newPairings = solve_subproblem_multi_label(adjList, M, dualVariables, costParams);
+fprintf("Subproblem Solved (%.2f seconds)\n", toc(subproblemStartTime));
 
+% Report total execution time
+fprintf("Total Execution Time: %.2f seconds\n", toc);
